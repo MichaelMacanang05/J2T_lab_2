416b5d5114eec2171e691d1d76047e102fcaee25
Gabriel John M. Gallano <123729441+MichaelMacanang05@users.noreply.github.com>
2023-02-10 19:53:17 +0800

    php database connection done

--- index.php
+++ index.php
@@ -2,20 +2,28 @@
 $servername = "localhost";
 $username = "Michael";
 $password = "Michael";
+$dbname = "Macanang";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
-$sql = "CREATE DATABASE Macanang";
+// sql to create table
+$sql = "CREATE TABLE Macanang (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
 if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+  echo "Table Macanang created successfully";
 } else {
-  echo "Error creating database: " . $conn->error;
+  echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
